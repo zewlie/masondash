d06be10bcfd530dc05abb25446298406ffa7df73
Mason Hartman <zewlie@gmail.com>
2016-02-21 10:36:21 -0800

    Added db queries to server file; need to test in templates.

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 """Server for the life dash."""
 
 import random
+from datetime import datetime, timedelta
+
+from model import connect_to_db, db
+from model import Pomodoro, PomoMetric, Daily, DailyDone, Food, Exercise, Factor
 
 from jinja2 import StrictUndefined
 from flask import Flask, Markup, render_template, redirect, request, flash, session, jsonify, url_for, abort
@@ -26,9 +30,31 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Home / main dash."""
 
-    # TODO: grab data from db
+    now = datetime.now()
 
-    return render_template('index.html')
+    # Grab current pomodoro + the metrics and dailies we'll need to build out the dash
+
+    current_pomo = db.session.query(Pomodoro).filter(Pomodoro.finish is None).first()
+    metrics = db.session.query(PomoMetric).all()
+    dailies = db.session.query(Daily).all()
+
+    # everything done in the last 12 hours should display
+
+    pomos_done = db.session.query(Pomodoro).filter((now - Pomodoro.finish) < timedelta(hours=12)).all()
+    dailies_done = db.session.query(DailyDone).filter((now - DailyDone.time) < timedelta(hours=12)).all()
+
+    food = db.session.query(Food).filter((now - Food.time) < timedelta(hours=12)).all()
+    exercise = db.session.query(Exercise).filter((now - Exercise.time) < timedelta(hours=12)).all()
+    factors = db.session.query(Factor).filter((now - Factor.time) < timedelta(hours=12)).all()
+
+    return render_template('index.html', current_pomo=current_pomo,
+                                         metrics=metrics,
+                                         dailies=dailies,
+                                         pomos_done=pomos_done,
+                                         dailies_done=dailies_done,
+                                         food=food,
+                                         exercise=exercise,
+                                         factors=factors)
 
 
 if __name__ == "__main__":
