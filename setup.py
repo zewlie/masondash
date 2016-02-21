147a97ab3e4be52732ae6ac31fd7a44e71f49b5d
Mason Hartman <zewlie@gmail.com>
2016-02-20 23:17:21 -0800

    Added automated daily task creation to setup.

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 """Creates the database for the life dash."""
 
 from model import connect_to_db, db
+from model import Daily
 from server import app
+
+
+DAILY_LIST = [['Walk Kitty', 'Take Kitty for a walk.', 3],
+              ['15m Exercise', '15 minutes of high-intensity exercise.', 3],
+              ['CoQ 10', '200mg CoQ 10 per dose.', 2],
+              ['Vitamin D3', '1000mg Vitamin D3 per dose.', 2],
+              ['Calcium', '500mg Calcium per dose.', 2],
+              ['Green Tea', 'Drink LOTS of green tea!', 3]
+              ]
+
+
+def create_dailies(DAILY_LIST):
+    """Creates pre-defined daily tasks."""
+
+    for daily in DAILY_LIST:
+        daily = Daily(name=daily[0],
+                      desc=daily[1],
+                      qty=daily[2])
+
+        db.session.add(daily)
+
+    db.session.commit()
+
 
 if __name__ == "__main__":
     connect_to_db(app)
-
-    # In case tables haven't been created, create them
     db.create_all()
+
+    create_dailies()
